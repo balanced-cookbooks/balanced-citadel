63aa8a074b8d95eb62700ad7d6e7de49b2aa4db6
Noah Kantrowitz <noah@coderanger.net>
2013-12-15 18:24:09 -0800

    Fill out the metadata.

--- metadata.rb
+++ metadata.rb
@@ -17,6 +17,12 @@
 #
 
 name 'balanced-citadel'
-version '0.1.0'
+version '1.0.0'
+
+maintainer 'Noah Kantrowitz'
+maintainer_email 'noah@coderanger.net'
+license 'Apache 2.0'
+description 'Balanced-specific overrides for the citadel cookbook'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 
 depends 'citadel'
